49fded5661e6e7844674426db2b6cee16ab7bcde
avinash-vllbh <avallabhaneni@customink.com>
2024-04-23 07:57:19 -0400

    Commit lib folder to repo

--- examples/webpack/src/index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import parse from 'html-react-parser';
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(parse('<h1>HTMLReactParser loaded with Webpack</h1>'));
--- umd/index.ts
+++
@@ -1,6 +0,0 @@
-import * as HTMLReactParser from '../lib';
-
-const parse = HTMLReactParser.default;
-Object.assign(parse, HTMLReactParser);
-
-export default parse;
